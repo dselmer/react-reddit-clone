e2a878739c08219d1206b3eed0013c690d5f6f7c
dselmer <dselmer@icstars.org>
2024-05-28 08:13:40 -0500

    created search bar and cards component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import "./App.css";
+import React from "react";
+import SearchBar from "./components/navbar/SearchBar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import DisplayCard from "./components/DisplayCard";
 
-function App() {
-  return <></>;
+export default function App() {
+  return (
+    <>
+      <SearchBar />
+      <DisplayCard />
+    </>
+  );
 }
-
-export default App;
---
+++ src/components/navbar/SearchBar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Form from "react-bootstrap/Form";
+
+function SearchBar() {
+  return <Form.Control placeholder="search" type="text" id="inputText" />;
+}
+
+export default SearchBar;
