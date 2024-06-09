467b894946e900f22f094d983bd0d021a8eb2164
dselmer <dselmer@icstars.org>
2024-06-09 16:23:18 -0500

    added and adjusted pictures for sideview comonent

--- src/components/sideviewComponent/SideViewComponent.js
+++ src/components/sideviewComponent/SideViewComponent.js
@@ -1,10 +1,35 @@
 import React from "react";
+import Card from "react-bootstrap/Card";
 import styles from "./styles.css";
 
 export default function SideViewComponent() {
   return (
-    <div className="sideview-container">
-      <h1>this is only a tests</h1>
+    <div className="sideview-container-flex">
+      <div className="subreddit-title-container">
+        <h3 className="subreddit-title">Subreddits</h3>
+      </div>
+      <div className="profile-image-container">
+        <Card.Img
+          className="subreddit-profile-image"
+          variant="top"
+          src="https://placehold.jp/100x100.png"
+        />
+        <Card.Img
+          className="subreddit-profile-image"
+          variant="top"
+          src="https://placehold.jp/100x100.png"
+        />
+        <Card.Img
+          className="subreddit-profile-image"
+          variant="top"
+          src="https://placehold.jp/100x100.png"
+        />
+        <Card.Img
+          className="subreddit-profile-image"
+          variant="top"
+          src="https://placehold.jp/100x100.png"
+        />
+      </div>
     </div>
   );
 }
