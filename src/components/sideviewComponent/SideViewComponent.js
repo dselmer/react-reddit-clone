bb1e59f8140352d6d408ce24d4fc88ac827a2cc0
dselmer <dselmer@icstars.org>
2024-06-10 08:10:01 -0500

    properly adjusted sideview component

--- src/components/sideviewComponent/SideViewComponent.js
+++ src/components/sideviewComponent/SideViewComponent.js
@@ -6,29 +6,57 @@ export default function SideViewComponent() {
   return (
     <div className="sideview-container-flex">
       <div className="subreddit-title-container">
-        <h3 className="subreddit-title">Subreddits</h3>
+        <h6 className="subreddit-title">Subreddits</h6>
       </div>
       <div className="profile-image-container">
-        <Card.Img
-          className="subreddit-profile-image"
-          variant="top"
-          src="https://placehold.jp/100x100.png"
-        />
-        <Card.Img
-          className="subreddit-profile-image"
-          variant="top"
-          src="https://placehold.jp/100x100.png"
-        />
-        <Card.Img
-          className="subreddit-profile-image"
-          variant="top"
-          src="https://placehold.jp/100x100.png"
-        />
-        <Card.Img
-          className="subreddit-profile-image"
-          variant="top"
-          src="https://placehold.jp/100x100.png"
-        />
+        <div className="profile-details">
+          <Card.Img
+            className="subreddit-profile-image"
+            variant="top"
+            src="https://placehold.jp/100x100.png"
+          />
+          <span className="details">reddit profile name</span>
+        </div>
+        <div className="profile-details">
+          <Card.Img
+            className="subreddit-profile-image"
+            variant="top"
+            src="https://placehold.jp/100x100.png"
+          />
+          <span className="details">reddit profile name</span>
+        </div>
+        <div className="profile-details">
+          <Card.Img
+            className="subreddit-profile-image"
+            variant="top"
+            src="https://placehold.jp/100x100.png"
+          />
+          <span className="details">reddit profile name</span>
+        </div>
+        <div className="profile-details">
+          <Card.Img
+            className="subreddit-profile-image"
+            variant="top"
+            src="https://placehold.jp/100x100.png"
+          />
+          <span className="details">reddit profile name</span>
+        </div>
+        <div className="profile-details">
+          <Card.Img
+            className="subreddit-profile-image"
+            variant="top"
+            src="https://placehold.jp/100x100.png"
+          />
+          <span className="details">reddit profile name</span>
+        </div>
+        <div className="profile-details">
+          <Card.Img
+            className="subreddit-profile-image"
+            variant="top"
+            src="https://placehold.jp/100x100.png"
+          />
+          <span className="details">reddit profile name</span>
+        </div>
       </div>
     </div>
   );
